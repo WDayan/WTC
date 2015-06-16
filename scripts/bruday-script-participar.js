74c5a876c180ed77e8d28bec546835b7fc809fd3
Bruno Orsoletta <br-bruno@hotmail.com>
2015-06-16 18:42:31 -0300

    autocomplete v1

--- scripts/bruday-script-participar.js
+++ scripts/bruday-script-participar.js
@@ -6,73 +6,6 @@ var marker2; 		/* atual  */
 var controle1 = 0;
 var controle2 = 0;
 
-/*
-function geocodePosition(pos) {
-  geocoder.geocode({
-    latLng: pos
-  }, function(responses) {
-    if (responses && responses.length > 0) {
-      updateMarkerAddress(responses[0].formatted_address);
-    } else {
-      updateMarkerAddress('Cannot determine address at this location.');
-    }
-  });
-}
-
-function updateMarkerStatus(str) {
-  document.getElementById('markerStatus').innerHTML = str;
-}
-
-function updateMarkerPosition(latLng) {
-  document.getElementById('info').innerHTML = [
-    latLng.lat(),
-    latLng.lng()
-  ].join(', ');
-}
-
-function updateMarkerAddress(str) {
-  document.getElementById('address').innerHTML = str;
-}
-
-function initialize() {
-  var latLng = new google.maps.LatLng(-34.397, 150.644);
-  var map = new google.maps.Map(document.getElementById('mapCanvas'), {
-    zoom: 8,
-    center: latLng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var marker = new google.maps.Marker({
-    position: latLng,
-    title: 'Point A',
-    map: map,
-    draggable: true
-  });
-  
-  // Update current position info.
-  updateMarkerPosition(latLng);
-  geocodePosition(latLng);
-  
-  // Add dragging event listeners.
-  google.maps.event.addListener(marker, 'dragstart', function() {
-    updateMarkerAddress('Dragging...');
-  });
-  
-  google.maps.event.addListener(marker, 'drag', function() {
-    updateMarkerStatus('Dragging...');
-    updateMarkerPosition(marker.getPosition());
-  });
-  
-  google.maps.event.addListener(marker, 'dragend', function() {
-    updateMarkerStatus('Drag ended');
-    geocodePosition(marker.getPosition());
-  });
-}
-*/
-/* haeuhaeuheauheaueheauheauahuahea
-hauahueahueahaheahueauhaeuhhuuaau */
-
-
-
 function initialize() {
   
   geocoder = new google.maps.Geocoder();
@@ -106,7 +39,33 @@ function initialize() {
   } else {
     /* Browser doesn't support Geolocation */
     handleNoGeolocation(false);
-  }
+  } 
+	/* autocomplete endereço 1 origem */
+	$("#txtEndereco").autocomplete({
+    	source: function (request, response) {
+            geocoder.geocode({ 'address': request.term }, function (results, status) {
+                response($.map(results, function (item) {
+                    return {
+                        label: item.formatted_address,
+                        value: item.formatted_address
+                    }
+                }));
+            })
+        }
+    });
+	/* autocomplete endereço 2 atual */
+    	$("#txtEndereco2").autocomplete({
+    	source: function (request, response) {
+            geocoder.geocode({ 'address': request.term }, function (results, status) {
+                response($.map(results, function (item) {
+                    return {
+                        label: item.formatted_address,
+                        value: item.formatted_address
+                    }
+                }));
+            })
+        }
+    });
 
 }
 /* fim função initialize() */
@@ -220,10 +179,3 @@ function handleNoGeolocation(errorFlag) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize); 	/* chamada da função initialize */
-
-
-
-
-
-
-
